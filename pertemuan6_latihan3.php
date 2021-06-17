3907d33b568a579e676ce80b064ead93f1e8d07a
rezaasb <80372709+rezaasb@users.noreply.github.com>
2021-06-17 21:50:19 +0700

    Add files via upload

---
+++ pertemuan6_latihan3.php
@@ -0,0 +1,10 @@
+<?php
+$UsiaKaryawan["adi"] = "19";
+$UsiaKaryawan["budi"] = "18";
+$UsiaKaryawan["cinta"] = "20";
+$UsiaKaryawan["didi"] = "21";
+$UsiaKaryawan["ego"] = "20";
+foreach ($UsiaKaryawan as $Nama => $umur) {
+    echo "Nama Karyawan: $Nama, Usia: $umur" . " th <br>";
+}
+?>
